de1941374a24749e2ae7a3e364b308df80703601
Соколов Глеб <g.sokolov@21vek.local>
2022-12-09 18:35:43 +0300

    rhf

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -9,7 +9,7 @@ interface IFormInputs {
 }
 
 const schema = yup.object({
-  email: yup.string().email('Не правильный email'),
+  email: yup.string().email('Не правильный email').required(),
   password: yup.string()
   .required('Обязательное поле') 
   .min(8, 'Пароль слишком короткий - минимум 8 знаков.')
@@ -17,36 +17,42 @@ const schema = yup.object({
 }).required();
 
 const Login = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm<IFormInputs>({
-    resolver: yupResolver(schema)
+  const { register, handleSubmit, formState: { errors, isValid }, reset } = useForm<IFormInputs>({
+    resolver: yupResolver(schema),
   });
+
   const onSubmit = (data: IFormInputs) => {
-      console.log(JSON.stringify(data))
-    };
+      alert(JSON.stringify(data))
+      reset()
+  };
 
   return (
     <>
       <HeaderForm />
       <form onSubmit={handleSubmit(onSubmit)} className='bg-slate-400 w-min mx-auto'>
-        <div className='px-[10px] mb-[37px]'>
+        <div className='px-[10px] '>
           <p className='font-[600] text-[20px] text-[#443E3E] leading-[23.87px] mb-[10px]'>E-mail</p>
           <input 
-            placeholder='Введите имя' 
+            placeholder='Введите имя'
             type='text' 
             {...register("email")} 
             className='outline-none w-[412px] h-[60px] py-[18px] px-[20px] rounded-[7px]' />
-          <p className='text-red-600'>{errors.email?.message}</p>
+            <div className='h-[30px]'>
+              <p className='text-red-600'>{errors.email?.message}</p>
+            </div>
         </div>
-        <div className='px-[10px] mb-[37px]'>
+        <div className='px-[10px]'>
           <p className='font-[600] text-[20px] text-[#443E3E] leading-[23.87px] mb-[10px]' >Пароль</p>
           <input 
             placeholder='Введите имя' 
             type='password' 
             {...register("password")} 
             className='outline-none w-[412px] h-[60px] py-[18px] px-[20px] rounded-[7px]'/>
-          <p className='text-red-600'>{errors.password?.message}</p>
+            <div className='h-[30px]'>
+              <p className='text-red-600'>{errors.password?.message}</p>
+            </div>
         </div>        
-        <input type="submit" />
+        <input type="submit" disabled={!isValid} className={`${ isValid === true ? ' bg-[#FFB700]': ' bg-[#836824]'} w-full h-[40px] `}/>
       </form>
     </>
   );
