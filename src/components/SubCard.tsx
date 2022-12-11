9bf92ad848f2628deb3cec670ac99cb8d94939d9
Соколов Глеб <g.sokolov@21vek.local>
2022-12-11 19:47:17 +0300

    fix

--- src/components/SubCard.tsx
+++ src/components/SubCard.tsx
@@ -7,24 +7,26 @@ export const SubCard = ({subsData}: subsDataType) => {
   const {text, price, top='null'} = subsData
 
   return (
-    <div className='
-     bg-white h-[126px] w-[288px] py-[24px] px-[32px]
-      md:h-[207px] md:w-[533px] md:py-[36px] md:px-[32px] relative overflow-hidden border-[1px] border-[#fafafa] hover:border-[1px] cursor-pointer hover:border-[#FFB700]
-      flex flex-col justify-between shadow-2xl rounded-[20px]'>
-        {
-          top === 'top' ? (
-            <div className='
-              flex justify-center items-center
-              font-bodyalt absolute bg-[#FFB700] w-[150px] h-[18px] translate-x-[147px] -translate-y-[7px] 
-              font-[400] text-[10px] text-center md:text-[16px] md:h-[42px] md:w-[220px] md:translate-x-[335px] md:-translate-y-[12px]  text-[#FAFAFA] rotate-[38deg] 
-              group-hover/card:bg-white group-hover/card:text-[#1F2117]'>
-                лучший выбор
-            </div>
-          ) : null
-        }
-      <div className='flex flex-col justify-center gap-[24px]'>
-        <p className='font-[600] text-[24px] leading-[28.18px] md:text-[40px] md:leading-[46.96px] text-[#1F2117] text-center'>{price}</p>
-        <p className='font-bodyalt font-[400] text-[14px] leading-[16.71px] md:text-[22px] md:leading-[32px] text-[#777872] text-center'>{text}</p>
+    <div className='relative overflow-hidden rounded-[20px] shadow-xl' >
+      <div className='
+      bg-white h-[126px] w-[288px] py-[24px] px-[32px]
+        md:h-[207px] md:w-[533px] md:py-[36px] md:px-[32px] overflow-hidden border-[1px] border-transparent hover:border-[1px] cursor-pointer hover:border-[#FFB700]
+        flex flex-col justify-between rounded-[20px]'>
+          {
+            top === 'top' ? (
+              <div className='
+                flex justify-center items-center
+                font-bodyalt absolute bg-[#FFB700] w-[150px] h-[18px] translate-x-[147px] -translate-y-[7px] 
+                font-[400] text-[10px] text-center md:text-[16px] md:h-[42px] md:w-[220px] md:translate-x-[335px] md:-translate-y-[12px]  text-[#FAFAFA] rotate-[38deg] 
+                group-hover/card:bg-white group-hover/card:text-[#1F2117]'>
+                  лучший выбор
+              </div>
+            ) : null
+          }
+        <div className='flex flex-col justify-center gap-[24px]'>
+          <p className='font-[600] text-[24px] leading-[28.18px] md:text-[40px] md:leading-[46.96px] text-[#1F2117] text-center'>{price}</p>
+          <p className='font-bodyalt font-[400] text-[14px] leading-[16.71px] md:text-[22px] md:leading-[32px] text-[#777872] text-center'>{text}</p>
+        </div>
       </div>
     </div>
   )
--- src/layouts/Feedback.tsx
+++ src/layouts/Feedback.tsx
@@ -1,5 +1,4 @@
 import React, {useState, useRef} from 'react'
-import { AiFillStar } from 'react-icons/ai'
 
 
 type dataT = {
@@ -12,9 +11,9 @@ type dataT = {
 type DataType = dataT[]
 
 const data: DataType = [
-  {text:'Пользуюсь уже более 4 лет. Приложение очень крутое.есть все необходимые упражнения, легко составлять планы тренировок, большой выбор готовых тренировок!', img: require('../images/feedbackphoto.png'), name: 'Игорь Василевский', prof: 'Фитнес-тренер', rate: 5 },
-  {text:'Пользуюсь уже более 4 лет. Приложение очень крутое.есть все необходимые упражнения, легко составлять планы тренировок, большой выбор готовых тренировок!', img: require('../images/feedbackphoto.png'), name: 'Игорь Василевский', prof: 'Фитнес-тренер', rate: 5 },
-  {text:'Пользуюсь уже более 4 лет. Приложение очень крутое.есть все необходимые упражнения, легко составлять планы тренировок, большой выбор готовых тренировок!', img: require('../images/feedbackphoto.png'), name: 'Игорь Василевский', prof: 'Фитнес-тренер', rate: 5 },
+  {text:'Удобное отслеживание прогресса. хороший мотиватор делать лучше и больше. Неплохой набор действительно бесплатных функций. ', img: require('../images/feedbackphoto.png'), name: 'Игорь Василевский', prof: 'Фитнес-тренер', rate: 5 },
+  {text:'Пользуюсь уже более 4 лет. Приложение очень крутое.есть все необходимые упражнения, легко составлять планы тренировок, большой выбор готовых тренировок!', img: require('../images/feedbackphoto.png'), name: 'Cody Fisher', prof: 'Фитнес-тренер', rate: 5 },
+  {text:'Пользуюсь уже более 4 лет. Приложение очень крутое.есть все необходимые упражнения, легко составлять планы тренировок, большой выбор готовых тренировок!', img: require('../images/feedbackphoto.png'), name: 'Marvin McKinney', prof: 'Фитнес-тренер', rate: 5 },
   {text:'Пользуюсь уже более 4 лет. Приложение очень крутое.есть все необходимые упражнения, легко составлять планы тренировок, большой выбор готовых тренировок!', img: require('../images/feedbackphoto.png'), name: 'Игорь Василевский', prof: 'Фитнес-тренер', rate: 5 },
   {text:'Пользуюсь уже более 4 лет. Приложение очень крутое.есть все необходимые упражнения, легко составлять планы тренировок, большой выбор готовых тренировок!', img: require('../images/feedbackphoto.png'), name: 'Игорь Василевский', prof: 'Фитнес-тренер', rate: 5 },
   {text:'Пользуюсь уже более 4 лет. Приложение очень крутое.есть все необходимые упражнения, легко составлять планы тренировок, большой выбор готовых тренировок!', img: require('../images/feedbackphoto.png'), name: 'Игорь Василевский', prof: 'Фитнес-тренер', rate: 5 },
@@ -26,38 +25,35 @@ const data: DataType = [
   {text:'Пользуюсь уже более 4 лет. Приложение очень крутое.есть все необходимые упражнения, легко составлять планы тренировок, большой выбор готовых тренировок!', img: require('../images/feedbackphoto.png'), name: 'Игорь Василевский', prof: 'Фитнес-тренер', rate: 5 },
 ]
 
-export const Feedback = () => {
-  const [active, setActive] = useState<number | null>(null) 
+export const Feedback = () => { 
   const [size, setSize] = useState<number>(0)
   const ref = useRef<HTMLDivElement | null>(null)
 
-  const activeButton =(i: number) => {
-    setActive(i)
-  }
+  const userBlock = Number(ref?.current?.getBoundingClientRect().width)
+
   const right = () =>{
-    active! >= data.length-1 ? setActive(0) : setActive(prev => prev! === null ? 0: prev + 1)
+    if(size <= (userBlock*data.length - userBlock*4)){
+      setSize(prev => prev + userBlock)
+    }
   }
   const left = () =>{
-    active! <= 0 ? setActive(data.length-1) : setActive(prev => prev! - 1)
+    if(size !== 0){
+      setSize(prev => prev - userBlock)
+    }
   }
-
-  const trLength = active! >4 ? `${(size+20)*(active!-3)}`: 0
-  const sizeCarousel = trLength >= 2200? '2200px' : `${trLength}px`
 
   const counterStars = (rate: number): string[] => {
     return ' '.repeat(rate).split('')
   } 
 
-
   React.useEffect(() => {
     if(ref.current) {
-      setSize(ref.current.getBoundingClientRect().width)
+      setSize(0)
     }
   },[])
-  
 
   return (
-    <div className='mb-[50px] gap-[10px] md:gap-[15px] md:mb-[50px] md:overflow-hidden'>
+    <div className='mb-[72px] gap-[10px] md:gap-[15px] md:mb-[150px] md:overflow-hidden'>
       <div className='flex flex-row mb-[32px] md:mb-[64px] space-x-[200px] justify-between'>
         <div className='font-[600] text-[31px] leading-[37.57px] md:text-[64px] text-[#1F2117] md:leading-[75.14px] '>Нас любят и ценят</div>
         <div className='hidden md:flex md:flex-row md:gap-[10px]'>
@@ -78,33 +74,35 @@ export const Feedback = () => {
         </div>
       </div>
       <div 
-        className='flex flex-row gap-[16px] md:h-[620px] duration-[600ms] snap-mandatory snap-x overflow-scroll md:snap-none md:overflow-visible '
-        style={{transform: `translateX(-${sizeCarousel})`}}>
+        className='flex flex-row gap-[16px] duration-[600ms] snap-mandatory snap-x overflow-scroll md:snap-none md:overflow-visible'
+        style={{transform: `translateX(-${size}px)`}}>
       {
         data.map((e,i) => {
           return(
-            <div key={i} className='snap-center'>
-              <div key={i} className='w-[226px] h-[256px] rounded-[20px] p-[16px] bg-white'>
-                <div className='font-bodyalt font-[600] h-[140px] text-[14px] text-[#1F2117] leading-[16.71px] mb-[17.4px]'>
+            <div ref={ref} key={i} className='snap-center'>
+              <div key={i} className='w-[226px] h-[256px] rounded-[20px] p-[16px] bg-white md:w-[500px] md:h-[319px] md:p-[24px] md:flex md:flex-col md:justify-between'>
+                <div className='font-bodyalt tracking-[0.02em] font-[600] h-[140px] text-[14px] text-[#1F2117] leading-[16.71px] mb-[17.4px] md:text-[22px] md:leading-[32px] '>
                   {e.text}
                 </div>
-                <div>
-                  <div className='mb-[17.4px] flex flex-row' >
+                <div className='md:grid md:grid-cols-2'>
+                  <div className='mb-[17.4px]  flex flex-row md:order-2 md:justify-end md:items-end md:m-[0px]' >
                     {
                       counterStars(e.rate).map((_,i) => {
                         return (
                           <div key={i}>
-                            <AiFillStar fill='#FFB700'/>
+                            <svg className='w-[13.88px] h-[13.2px] md:w-[20.82px] md:h-[19.8px]' width="22" height="20" viewBox="0 0 22 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                              <path d="M11.0961 0.105469L14.3135 6.62453L21.5077 7.66991L16.3019 12.7443L17.5308 19.9094L11.0961 16.5265L4.66143 19.9094L5.89035 12.7443L0.68457 7.66991L7.87879 6.62453L11.0961 0.105469Z" fill="#FFB700"/>
+                            </svg>
                           </div>
                         )
                       })
                     }
                   </div>
-                  <div className='flex flex-row gap-[8px]'>
-                    <img src={e.img} alt={e.name} className='w-[36px] h-[36px]'/>
+                  <div className='flex flex-row gap-[8px] md:order-1 md:gap-[14px]'>
+                    <img src={e.img} alt={e.name} className='rounded-full w-[36px] h-[36px] md:w-[64px] md:h-[64px]'/>
                     <div className='flex flex-col justify-center'>
-                      <p className='font-bodyalt font-[400] text-[12px] leading-[14px] text-[#1F2117] mb-[2px]'>{e.name}</p>
-                      <p className='font-bodyalt font-[400] text-[10px] leading-[12px] text-[#777872]'>{e.prof}</p>
+                      <p className='w-max font-bodyalt font-[400] text-[12px] leading-[14px] text-[#1F2117] mb-[2px] md:font-[500] md:text-[18px] md:leading-[21.48px] md:mb-[6px]'>{e.name}</p>
+                      <p className='font-bodyalt font-[400] text-[10px] leading-[12px] text-[#1F2117]/60 md:text-[14px] md:leading-[16.71px]'>{e.prof}</p>
                     </div>
                   </div>
                 </div>
